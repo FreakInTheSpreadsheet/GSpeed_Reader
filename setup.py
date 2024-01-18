cc160e4d9909e933bcafc8abcf2ac9ea4e69f22a
Grant DeCapua <grantdecap@proton.me>
2024-01-18 17:16:02 -0500

    Remove Setup file

--- setup.py
+++
@@ -1,8 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(
-    name="Grant's Speed Reader",
-    version="0.5.0",
-    description="Definitely not a $pr33der ripoff",
-    executables=[Executable("Speed_Reader.py", icon=".\FukSpreeder\cropped_logo.ico")]
-)
